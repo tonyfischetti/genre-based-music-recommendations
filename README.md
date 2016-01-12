ac5c76a37b5f33662cf43ca92a96ae03269461f2
Tony Fischetti <tony.fischetti@gmail.com>
2016-01-11 22:44:32 -0500

    final changes

--- README.md
+++ README.md
@@ -5,14 +5,12 @@
 
 ## Research on combining open source "tag" (genre) metadata to inform musical artist recommendations
 
-`make-recommendation-json.R` reads the artist/manually-labeled-genre pairs from
-`artists.csv`, uses the last.fm API to get a list of stranger-submitted tags,
+See [this blog post](http://www.onthelambda.com/2016/01/11/genre-based-music-recommendations-using-open-data-and-the-problem-with-recommender-systems/)
+
+Uses the last.fm API to get a list of stranger-submitted tags,
 using jaccard's similarity index to quantify the similarity between each
-pair of 70 artists, and spits out pseudo-json in `json.txt` which requires
-minimal editing to make valid json. `index.html` then reads the valid json,
-`artists.json` to construct a d3 visualization of the artist similarity
+pair of 70 artists, and spits out `artists.json`. `index.html` then reads
+that json to construct a d3 visualization of the artist similarity
 network. The d3 visualization includes tooltips that give artist
 recommendations (the three most similar other artists).
 
-Why I didn't write the R script to spit out valid JSON right from the get
-is still unclear. I'd wager it's laziness.
--- make-recommendation-json.R
+++ make-recommendation-json.R
@@ -9,15 +9,8 @@
 ##                                                       ##
 ###########################################################
 
-# workspace cleanup
-rm(list=ls())
 
-# options
-options(echo=TRUE)
 options(stringsAsFactors=FALSE)
-
-# cli args
-args <- commandArgs(trailingOnly=TRUE)
 
 # libraries
 library(dplyr)
@@ -26,9 +19,32 @@ library(memoise)
 library(jsonlite)
 
 
-##
-## FUNCTIONS
-##
+
+
+
+ARTIST_LIST <- c("Belle and Sebastian", "The Smiths", "The Magnetic Fields",
+                 "P:ano", "The Cure", "Stars", "Bad Religion",
+                 "Elvis Costello", "The Beatles", "Radiohead",
+                 "Guided by Voices", "NOFX", "The Strokes", "Morrissey",
+                 "Joy Division", "New Order", "Depeche Mode", "Wu-Tang Clan",
+                 "The New Pornographers", "Peter Tosh", "Cocteau Twins",
+                 "The Sisters of Mercy", "Lowlife", "Tokyo Police Club",
+                 "Johann Sebastian Bach", "Echo and the Bunnymen", "Beirut",
+                 "Gary Numan", "They Might Be Giants", "KRS-One",
+                 "Little Nemo", "Rancid", "The Coup", "And Also The Trees",
+                 "Pet Shop Boys", "No Kids", "Dead Can Dance", "Arcade Fire",
+                 "dead prez", "Kate Bush", "INDK", "The Divine Comedy",
+                 "The Velvet Underground", "Anti-Flag", "Broken Social Scene",
+                 "Metric", "Talking Heads", "Pixies", "Public Image Ltd.",
+                 "Sex Pistols", "The Psychedelic Furs", "The Church",
+                 "Black Uhuru", "Bob Marley", "A Tribe Called Quest",
+                 "Busta Rhymes", "Leaders of the New School", "Choking Victim",
+                 "The National", "Ghostface Killah", "Rakim", "De La Soul",
+                 "Public Enemy", "Raekwon", "Boogie Down Productions",
+                 "Operation Ivy", "Catch 22", "The Kinks", "Bauhaus",
+                 "Masta Killa")
+
+
 
 create_artist_query_url_lfm <- function(artist_name){
   prefix <- "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist="
@@ -42,128 +58,78 @@ get_tag_frame_lfm <- function(an_artist){
   print(paste0("Attempting to fetch: ", an_artist))
   artist_url <- create_artist_query_url_lfm(an_artist)
   json <- fromJSON(artist_url)
-  retframe <- data.frame(json$toptags$tag[,c("count", "name")])
-  names(retframe)[1] <- an_artist
-  return(retframe)
+  return(as.vector(json$toptags$tag[,"name"]))
 }
 
 mem_get_tag_frame_lfm <- memoise(get_tag_frame_lfm)
 
-
-# Failed experiment to improve recommendations
-# by employing degree of tag fit
-sim_coef <- function(frame1, frame2){
-  comb <- merge(frame1, frame2, all=TRUE)
-  comb <- select(comb, -name)
-  comb[is.na(comb)] <- 0
-  denom <- sum(comb[,1]) * sum(comb[,2])
-  numer <- sum(comb[,1] * comb[,2])
-  return(numer/denom)
-}
-mem_sim_coef <- memoise(sim_coef)
+mem_get_tag_frame_lfm("La Banda Gorda")
 
 
 
-jaccard_index <- function(frame1, frame2){
-  one <- frame1[["name"]]
-  two <- frame2[["name"]]
-  length(intersect(one, two))/length(union(one, two))
+artists_tags <- sapply(ARTIST_LIST, mem_get_tag_frame_lfm)
+names(artists_tags) <- ARTIST_LIST
+
+
+
+cmbs <- combn(ARTIST_LIST, 2)
+comparisons <- data.frame(t(cmbs))
+names(comparisons) <- c("artist1", "artist2")
+
+
+jaccard_index <- function(tags1, tags2){
+  length(intersect(tags1, tags2))/length(union(tags1, tags2))
 }
 
-mem_jaccard_index <- memoise(jaccard_index)
-
-lookup_number <- function(name) which(name==our_artists)-1
-
-get_genre_code <- function(name) ARTISTS[which(ARTISTS$artists==name),3]
-
-get_top_3 <- function(the_pairs, artist, key){
-  the_pairs %<>% filter(artist1==artist | artist2==artist)
-  another <- the_pairs
-  # don't even look at this, it's very late
-  another %<>%
-    rename(artisttmp = artist2) %>%
-    rename(artist2 = artist1) %>%
-    rename(artist1 = artisttmp) %>%
-    select_("artist1", "artist2", key) %>%
-    rbind(the_pairs) %>%
-    filter(artist1 == artist) %>%
-    arrange_(key)
-  get_name <- function(arow){
-    if(arow[[key]] > 0.25)
-      return(arow[2])
-    return("None")
-  }
-  first <- get_name(another[69,])
-  second <- get_name(another[68,])
-  third <- get_name(another[67,])
-  return(as.vector(unlist(c(first, second, third))))
-}
-
-##
-##
-##
-
-
-ARTISTS <- read.csv("./artists.tsv", sep="\t")
-
-our_artists <- ARTISTS$artists
-our_artists_tags <- lapply(our_artists, mem_get_tag_frame_lfm)
-names(our_artists_tags) <- our_artists
-
-codes <-  as.numeric(factor(ARTISTS$my_genres))
-ARTISTS$genre.code <- as.vector(codes)
-
-
-
-cbms <- combn(our_artists, 2)
-
-art_pairs <- data.frame(t(rbind(cbms)))
-names(art_pairs) <- c("artist1", "artist2")
-
-art_pairs$jacc <- apply(art_pairs, 1,
+comparisons$similarity <- apply(comparisons, 1,
   function(arow){
-    mem_jaccard_index(our_artists_tags[[unlist(arow[1])]],
-                      our_artists_tags[[unlist(arow[2])]])
+    jaccard_index(artists_tags[[unlist(arow[1])]],
+                  artists_tags[[unlist(arow[2])]])
   }) 
 
 
 
+get_top_n <- function(comparisons, N, artist, threshold){
+  comparisons %<>%
+    filter(artist1==artist | artist2==artist) %>%
+    arrange(desc(similarity))
+  other_artist <- ifelse(comparisons$similarity>threshold,
+                         ifelse(comparisons$artist1==artist,
+                                comparisons$artist2, comparisons$artist1),
+                         "None")
+  return(other_artist[1:N])
+}
 
 
-sink("json.txt")
+# need "name,group,first,second,third"
+nodes <- sapply(ARTIST_LIST, function(x) get_top_n(comparisons, 3, x, 0.25))
+nodes <- data.frame(t(nodes))
+names(nodes) <- c("first", "second", "third")
+nodes$name <- row.names(nodes)
+row.names(nodes) <- NULL
+nodes$group <- 1
 
-cat("{\n    \"nodes\": [\n")
 
-lapply(our_artists, function(x){
-       recs <- get_top_3(art_pairs, x, "jacc")
-       cat(paste0("        { \"name\": \"", x, "\", \"group\": ",
-                  get_genre_code(x), ", \"first\": \"", recs[1],
-                  "\", \"second\": \"", recs[2], "\", \"third\": \"",
-                  recs[3], "\" },\n"))
-})
+# need "source,target,node1,node2,weight"
 
-cat("    ],\n    \"links\": [\n")
+# find the 0-indexed index
+lookup_number <- function(name) which(name==ARTIST_LIST)-1
 
-apply(art_pairs, 1, function(arow){
-      if(arow[5] > 0.25){
-        cat(paste0("        {\"source\": ",
-                   lookup_number(unlist(arow[1])),
-                   ", \"target\": ",
-                   lookup_number(unlist(arow[2])),
-                   ", \"weight\": ",
-                   arow[5],
-                   ", \"node1\": \"",
-                   arow[1],
-                   "\", \"node2\": \"",
-                   arow[2],
-                   "\"},\n")) 
-      } else{
-        cat("")
-      } })
- cat("\n    ]\n}\n")
+strong_links <- comparisons %>%
+  filter(similarity > 0.25) %>%
+  rename(node1 = artist1, node2 = artist2, weight=similarity)
+strong_links$source <- sapply(strong_links$node1, lookup_number)
+strong_links$target <- sapply(strong_links$node2, lookup_number)
+
+
+
+
+object <- list("nodes"=nodes,
+               "links"=strong_links)
+
+sink("artists.json")
+toJSON(object, dataframe="rows", pretty=TRUE)
 sink()
-
-
 
 
 
